aa3973558312272022b95ae3ec5a20a1c763c1c0
andrevale99 <andrefvale@gmail.com>
2022-03-21 09:22:38 -0300

    Update

--- CN_cpp/main.cpp
+++ CN_cpp/main.cpp
@@ -16,24 +16,14 @@ int main()
     cin >> r >> c;
 
     CN x(r,c);
-    CN y(r,c);
-    
-    x.fill(2.);
-    y.fill(3.214531);
-    
-    cout << "X:\n" << x;
-    cout << "Y:\n" << y;
+    x.fill(3.32);
 
-    cout << x*y;
+    CN y(x);
+    cout << y;
 
-    cout << y.save("teste.txt") << '\n';
-
-    CN teste = x+y;
-
-    cout << teste;
-    cout << teste.read("teste2.txt") << '\n';
-    cout << teste;
-    
+    cout << y*x;
+    cout << y+y;
+    cout << y-y;
 
     return 0;
 }
